67215ada8a483e2dc1819b1bf9e3bc359045ca73
Marty Mammel <mmammel@wiley.com>
2018-07-05 10:22:43 -0400

    Section 11 Lecture 192

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
 class Blog extends Component {
     render () {
-       
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><NavLink 
+                                to='/'
+                                exact
+                                activeClassName='my-active'
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'}}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'}}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path='/' exact render={() => <h1>Home</h1>} />
+                <Route path='/' render={() => <h1>Home2</h1>} /> */}
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -10,6 +10,10 @@ class NewPost extends Component {
         author: 'Max'
     }
 
+    componentDidMount () {
+        console.log(this.props);
+    }
+
     postDataListener = () => {
         const data = {
             title: this.state.title,
